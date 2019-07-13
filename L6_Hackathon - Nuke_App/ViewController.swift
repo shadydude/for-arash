3c9fdcd6817962f9002f24ce04ddae971050afe1
Shayden <shayden@shaydens-mac.local>
2019-07-13 10:04:18 +0800

    Initial Commit

---
+++ L6_Hackathon - Nuke_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  L6_Hackathon - Nuke_App
+//
+//  Created by Shayden Wong on 13/7/19.
+//  Copyright © 2019 Shayden Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
